b7ebbcebcdc5e14a21ca8c39b3b5ff37e51e6511
Miwacer <il>
2025-05-08 01:13:42 +0300

    fix code on utrs.py

--- taxi/urls.py
+++ taxi/urls.py
@@ -45,14 +45,19 @@ urlpatterns = [
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
-    path("cars/<int:car_id>/add_or_remove/", add_or_remove_driver_on_car, name="car-assign"),
+    path(
+        "cars/<int:car_id>/add_or_remove/",
+        add_or_remove_driver_on_car,
+        name="car-assign"
+    ),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
     path(
         "drivers/<int:pk>/update-license/",
         DriverLicenseUpdateView.as_view(),
-        name="driver-update-license"),
+        name="driver-update-license"
+    ),
     path(
         "drivers/<int:pk>/delete/", DriverDeleteView.as_view(), name="driver-delete"
     ),
